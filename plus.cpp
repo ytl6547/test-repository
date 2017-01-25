22ec799fc320ecc7b69ca43f597fcad71408f1d0
Tianli Yu <tianliyu@usc.edu>
2017-01-24 22:20:19 -0800

    Added a cpp file.

---
+++ plus.cpp
@@ -0,0 +1,15 @@
+// plus.cpp
+//
+// Author: Tianli Yu
+// Inputs two numbers and outputs the sum of the two numbers
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+	return 0;
+}
